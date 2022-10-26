c6927ffd6a989db7e252ed914bc2cffd71307d41
lana2-2karla <allanakarla1702@gmail.com>
2022-10-26 16:28:06 -0300

    :sparkles: feat: implementa função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    list_text = txt_importer(path_file)
+    info_text = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(list_text),
+        "linhas_do_arquivo": list_text,
+    }
+    instance.enqueue(info_text)
+    return sys.stdout.write(f"{info_text}\n")
 
 
 def remove(instance):
