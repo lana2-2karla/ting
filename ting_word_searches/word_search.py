637f50b7c1b8cf3ff9b5a87a9b00c2160491d47c
lana2-2karla <allanakarla1702@gmail.com>
2022-10-27 18:42:57 -0300

    :construction: feat: implementa função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue):
+
+    ocorrencias_info = []
+
+    for index, text in enumerate(instance._data):
+        if word.lower() in text['linhas_do_arquivo'].lower():
+            dict_info_text = {
+                "palavra": word,
+                "arquivo": text["nome_do_arquivo"],
+                "ocorrencias": [{f"linha: {index + 1}"}]
+            }
+            dict_info_text.append(ocorrencias_info)
+    return ocorrencias_info
 
 
 def search_by_word(word, instance):
