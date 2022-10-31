bd57812559a299b47d6fee48d4736e63c60e46da
lana2-2karla <allanakarla1702@gmail.com>
2022-10-31 17:32:43 -0300

    :sparkles: feat: implementa funções exists_word e search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -4,17 +4,36 @@ from ting_file_management.queue import Queue
 def exists_word(word: str, instance: Queue):
 
     ocorrencias_info = []
-
-    for index, text in enumerate(instance._data):
-        if word.lower() in text['linhas_do_arquivo'].lower():
-            dict_info_text = {
-                "palavra": word,
-                "arquivo": text["nome_do_arquivo"],
-                "ocorrencias": [{f"linha: {index + 1}"}]
-            }
-            dict_info_text.append(ocorrencias_info)
+    for item in range(len(instance)):
+        file = instance.search(item)
+        dict_info_text = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for index, text in enumerate(file['linhas_do_arquivo']):
+            if word.lower() in text.lower():
+                dict_info_text['ocorrencias'].append({"linha": index + 1})
+        if dict_info_text["ocorrencias"]:
+            ocorrencias_info.append(dict_info_text)
     return ocorrencias_info
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    ocorrencias_info = []
+    for item in range(len(instance)):
+        file = instance.search(item)
+        dict_info_text = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for index, text in enumerate(file['linhas_do_arquivo']):
+            if word.lower() in text.lower():
+                dict_info_text['ocorrencias'].append({
+                    "linha": index + 1,
+                    "conteudo": text
+                })
+        if dict_info_text["ocorrencias"]:
+            ocorrencias_info.append(dict_info_text)
+    return ocorrencias_info
